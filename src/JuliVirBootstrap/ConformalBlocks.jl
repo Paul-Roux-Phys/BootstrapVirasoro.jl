cc15ee1522e24f63aa60e0d6a8ae0aab936a2960
Paul Roux <paul.roux@phys.ens.fr>
2024-10-14 16:49:51 +0200

    corrected typo in case of included files

--- src/JuliVirBootstrap/ConformalBlocks.jl
+++ src/JuliVirBootstrap/ConformalBlocks.jl
@@ -27,9 +27,9 @@ Block(c, chan, d::ConformalDimension, lr, Nmax; der=false) =
 Block(c, chan, V::Field, lr::Symbol) = Block(c, chan, V, lr, c.Nmax)
 Block(c, chan, V::Field) = Block(c, chan, V, c.Nmax)
 
-include("ConformalBlocks/Residues.jl")
+include("ConformalBlocks/residues.jl")
 include("ConformalBlocks/Correlations.jl")
 include("ConformalBlocks/BlocksSeries.jl")
 include("ConformalBlocks/prefactors.jl")
-include("COnformalBlocks/logarithmic.jl")
+include("ConformalBlocks/logarithmic.jl")
 include("ConformalBlocks/evaluate.jl")
