8303ad06ffe1178aa04c68530903466c428d1c16
Paul Roux <paul.roux92@gmail.com>
2025-06-05 10:36:09 +0200

    bootstrap converges

--- src/bootstrap_equations.jl
+++ src/bootstrap_equations.jl
@@ -1,9 +1,3 @@
-struct BootstrapMatrix{T}
-    unknowns::Channels{Vector{Tuple{Int, Field{T}}}}
-    LHS::Matrix{T}
-    RHS::Vector{T}
-end
-
 """
 TODO
 """
@@ -12,13 +6,33 @@ mutable struct StructureConstants{T}
     errors::Channels{Dict{Field{T}, T}}
 end
 
-mutable struct BootstrapSystem{T, U<:ChannelSpectrum{T}}
-    positions::Vector{T}
-    positions_cache::Channels{Vector{LRPositionCache{T}}}        # positions at which eqs are evaluated
-    spectra::Channels{U}    # channel spectra
-    blocks::Channels{Vector{Vector{T}}} # all blocks evaluated
-    matrix::BootstrapMatrix{T}  # matrix of equations
-    consts::StructureConstants{T}
+function StructureConstants{T}() where {T}
+    constants = Channels{Dict{Field{T}, T}}(Tuple(
+        Dict() for chan in (:s, :t, :u)
+    ))
+    errors = deepcopy(constants)
+    return StructureConstants{T}(constants, errors)
+end
+
+function Base.getproperty(c::StructureConstants, s::Symbol)
+    s === :fields && begin
+        consts = getfield(c, :constants)
+        return vcat([[V for V in keys(consts[chan])] for chan in keys(consts)]...)
+    end
+    getfield(c, s)
+end
+
+Base.getindex(c::StructureConstants, s::Symbol) = c.constants[s]
+
+function fix!(cnst, chan, field, value; error = 0)
+    cnst[chan][field] = value
+    cnst.errors[chan][field] = error
+end
+
+struct BootstrapMatrix{T}
+    unknowns::Channels{Vector{Tuple{Int, Field{T}}}}
+    LHS::Matrix{T}
+    RHS::Vector{T}
 end
 
 function BootstrapMatrix{T}() where {T}
@@ -51,6 +65,16 @@ function random_points(N; transfo=missing, square=true)
     end
     return transfo !== missing ? transfo.(res) : res
 end
+
+mutable struct BootstrapSystem{T, U<:ChannelSpectrum{T}}
+    positions::Vector{T}
+    positions_cache::Channels{Vector{LRPositionCache{T}}}        # positions at which eqs are evaluated
+    spectra::Channels{U}    # channel spectra
+    blocks::Channels{Vector{Vector{T}}} # all blocks evaluated
+    matrix::BootstrapMatrix{T}  # matrix of equations
+    consts::StructureConstants{T}
+end
+
 
 function evaluate_blocks(bs::Vector{Block{T}}, xs) where {T}
     nbs = length(bs)
@@ -224,3 +248,75 @@ function clear!(b::BootstrapSystem{T}) where {T}
     b.consts = StructureConstants(b.spectra)
     b.matrix = BootstrapMatrix{T}([chan for chan in keys(b.spectra)]);
 end
+
+function format_complex(z::Complex{<:Real})
+    real_str = @sprintf("%.5e", real(z))
+    imag_str = @sprintf("%.5e", abs(imag(z)))
+    sign = imag(z) < 0 ? "-" : "+"
+    buf = real(z) > 0 ? " " : ""
+    return "$buf$real_str $sign $(imag_str)im"
+end
+
+function Base.show(io::IO, c::StructureConstants{T}) where {T}
+    chans = keys(c.constants)
+    nondiags = Dict(
+        chan => sort(
+            [V for V in keys(c[chan]) if !isdiagonal(V)],
+            by=V -> (V.r, V.s)
+        )
+        for chan in chans
+    )
+    diags = Dict(
+        chan => sort(
+            [V for V in keys(c[chan]) if isdiagonal(V) && !isdegenerate(V)],
+            by=V -> real(total_dimension(V))
+        )
+        for chan in chans
+    )
+    degs = Dict(
+        chan => sort(
+            [V for V in keys(c[chan]) if isdegenerate(V)],
+            by=V -> real(total_dimension(V))
+        )
+        for chan in chans
+    )
+
+    for chan in sort(collect(chans), by=string)
+        # Collect all the labels for this channel
+        all_Vs = vcat(degs[chan], diags[chan], nondiags[chan])
+
+        # Find max width of label for alignment
+        max_label_width = if isempty(all_Vs)
+            0
+        else
+            maximum(length(string(V)) for V in all_Vs)
+        end
+
+        str_cst_col_width = if isempty(all_Vs)
+            0
+        else
+            length(format_complex(zero(T)))
+        end
+
+        # Print channel header
+        println(io, "Channel $(chan)")
+        println(io, repeat('=', 9 + length(string(chan))))
+        
+        # Print column headers
+        label1 = rpad("Fields", max_label_width)
+        label2 = rpad("Structure constants", str_cst_col_width)
+        label3 = rpad("Relative errors", str_cst_col_width)
+        println(io, "$label1 | $label2  | $label3")
+        println(io, repeat("-", max_label_width+2*str_cst_col_width))
+
+        # Print each row with alignment
+        for V in all_Vs
+            label = rpad(string(V), max_label_width)
+            value = format_complex(c[chan][V])
+            error = format_complex(c.errors[chan][V])
+            println(io, "$label | $value | $error")
+        end
+    end
+end
+
+Base.length(c::StructureConstants) = sum(length(c.constants[chan]) for chan in keys(c.constants))
