8e8c33459e617230d45a9390fd3ff81dbcb7ded1
Paul Roux <paul.roux92@gmail.com>
2026-07-16 04:53:39 +0200

    simplify syntax of example

--- examples/bootstrap.jl
+++ examples/bootstrap.jl
@@ -12,8 +12,8 @@ P    = big"0.67"
 VP   = Field(c, P=P) # the external diagonal field
 VPpm = [Field(c, P = P + pm*1/(2β)) for pm in (-1, 1)] # shifted fields
 Vd   = [Field(c, r=1, s=1, diagonal=true),
-      Field(c, r=1, s=3, diagonal=true),
-      Field(c, r=1, s=5, diagonal=true)] # degenerate fields
+        Field(c, r=1, s=3, diagonal=true),
+        Field(c, r=1, s=5, diagonal=true)] # degenerate fields
 # list of all fields that can go in channels
 # vcat concatenates arrays.
 V    = vcat(VPpm, Vd) 
@@ -23,18 +23,16 @@ Cor  = Correlation(V12, VP, V12, VP, 60) # define the correlation
 # We need to define ChannelSpectrum objects for each channel.
 # They are created as ChannelSpectrum(correlation, channel, listoffields, function)
 # where the function is a function that takes as input a field and outputs a block object.
-fs(V) = NCBlock(Cor, :s, V) # map fields to blocks
-Cs    = ChannelSpectrum(Cor, :s, VPpm, fs) # create s-channel spectrum
-ft(V) = NCBlock(Cor, :t, V)
-Ct    = ChannelSpectrum(Cor, :t, V, ft) # t-channel
-fu(V) = NCBlock(Cor, :u, V)
-Cu    = ChannelSpectrum(Cor, :u, Vd, fu) # u-channel
+f(V, chan) = NCBlock(Cor, chan, V) # map fields to blocks
+Cs    = ChannelSpectrum(Cor, :s, VPpm, V -> f(V, :s)) # create s-channel spectrum
+Ct    = ChannelSpectrum(Cor, :t, V,    V -> f(V, :t)) # t-channel
+Cu    = ChannelSpectrum(Cor, :u, V,    V -> f(V, :u)) # u-channel
 
-sol = solve_bootstrap(Channels(Cs, Ct, Cu)) # setup and solve the crossing equations
-sol.str_cst # return the structure constants
+sol = solve_bootstrap(Channels(Cs, Ct, Cu), rels=:tu) # setup and solve the crossing equations
+sol # return the structure constants
 
 # you can display the structure constants with
-# println(sol.str_cst)
+# println(sol)
 # or
-# show(stdout, sol.str_cst)
+# show(stdout, sol)
 # changing stdout to a file IO will print to a file instead.
